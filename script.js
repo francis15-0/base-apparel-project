37ffc7e0f2c9b1db9c550a9a225b7d10addcc593
francis15-0 <nnadiekwemakuo.11@gmail.com>
2023-08-23 01:54:35 -0400

    v9 updated the js functionality

--- script.js
+++ script.js
@@ -14,21 +14,33 @@
 function validate() {
     const wrapper = document.querySelector(".email-container");
     const email = document.querySelector(".email");
-    const paragraph = document.createElement("p");
-    const errorMessage = document.createTextNode("This is not a valid email");
+    const paragraph = document.getElementById("error-text");
     const errorLogo = document.querySelector("#err");
   
-    paragraph.appendChild(errorMessage);
+    
   
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if (!emailPattern.test(email.value)) {
-      wrapper.appendChild(paragraph);
+      paragraph.innerHTML="Please provide a valid email";
       console.log("here");
       errorLogo.classList.remove("error");
+      email.value="";
+      setTimeout(()=>{
+        email.value="";
+        paragraph.innerHTML="";
+        errorLogo.classList.add("error");
+      }, 1000);
       return false;
     }
     else{
+      paragraph.innerHTML="";
+      paragraph.innerHTML= "Thank you"
         errorLogo.classList.add("error");
+        setTimeout(function(){
+          paragraph.innerHTML="";
+          email.value="";
+        }, 1000);
+
     }
     return true;
   }
